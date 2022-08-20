d1c6ab90249c6dc3c68fcf7a308176fac2d5078e
mallick-portfolioE <tamal1714019@gmail.com>
2022-08-20 23:59:49 +0600

    added footer column2

---
+++ client/src/components/Footer/Column2.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Column2 = () => {
+  return (
+    <div>
+      <h4 className="text-base sm:text-lg font-robotoS text-white font-medium relative after:absolute after:top-8 mb-6 after:bg-primary after:left-0 after:w-12 after:h-0.5">
+        Useful Links
+      </h4>
+      <ul className="flex flex-col gap-2 text-sm text-textBase">
+        <li className="border-b border-dashed border-textBase pb-2 cursor-pointer">
+          Home Page
+        </li>
+        <li className="border-b border-dashed border-textBase pb-2 cursor-pointer">
+          About Us
+        </li>
+        <li className="border-b border-dashed border-textBase pb-2 cursor-pointer">
+          Our Mission
+        </li>
+        <li className="border-b border-dashed border-textBase pb-2 cursor-pointer">
+          Terms and Conditions
+        </li>
+        <li className="border-b border-dashed border-textBase pb-2 cursor-pointer">
+          FAQ
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Column2;
--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Column1 from "./Column1.jsx";
+import Column2 from "./Column2.jsx";
 
 const Footer = () => {
   return (
     <div className="bg-footerBg">
       <div className="container py-16 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 sm:gap-6 md:gap-8">
         <Column1 />
+        <Column2 />
       </div>
     </div>
   );
